a2174ab27f6e66bbae8cf51190b84221abaf4770
pparonson <preston.aronson@gmail.com>
2018-04-21 21:33:11 -0600

    a comparision of either examples to imperative code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const {nextCharForNumberString} = require("./dfwct");
 const {applyDiscount} = require("./rictasceub");
 const {right} = require("./eancwccbue");
+const {getPort} = require("./acoeectic");
 
 // console.log(nextCharForNumberString(" 64 "));
 
 // console.log(moneyToFloat("$5.25"));
 // console.log(applyDiscount("$5.25", "20%"));
+
+const result = getPort();
+console.log(result);
